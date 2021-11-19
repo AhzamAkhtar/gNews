8bd676457dbf81a03ebde166cceebd26456a3d66
Ahzam Akhtar <ahzam8186@gmail.com>
2021-11-19 21:46:04 +0530

    g-news

--- src/Nyt.jsx
+++ src/Nyt.jsx
@@ -2,10 +2,11 @@ import React,{useEffect} from "react";
 const Nyt=()=>{
     useEffect(()=>{
         const ftechApi=async()=>{
-            const url="https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=dlVR6N3WDcWAVYW05GQ5KmKlajpBJ0ck"
+            //const url="https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=dlVR6N3WDcWAVYW05GQ5KmKlajpBJ0ck"
+            const url="https://gnews.io/api/v4/search?q=t20 world cup&token=a3bd46c42dd12ae35906909a6c7b3556"
+            //const url="https://gnews.io/api/v4/top-headlines?token=a3bd46c42dd12ae35906909a6c7b3556"
             const response=await fetch(url)
             const resJson=await response.json()
-            console.log(resJson.results[1].title)
             console.log(resJson)
                
         }
@@ -15,7 +16,7 @@ const Nyt=()=>{
         <>
             <h1>hello</h1>
             <h2>edited</h2>
-            <h6>news</h6>
+            <h6>news night</h6>
         </>
     )
 }
