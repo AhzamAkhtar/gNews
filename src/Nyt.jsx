1292bc51a18ec5f1c0e75f49a70e996d2b77e9fa
Ahzam Akhtar <ahzam8186@gmail.com>
2021-11-19 16:40:48 +0530

    again edited

--- src/Nyt.jsx
+++ src/Nyt.jsx
@@ -5,13 +5,17 @@ const Nyt=()=>{
             const url="https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=dlVR6N3WDcWAVYW05GQ5KmKlajpBJ0ck"
             const response=await fetch(url)
             const resJson=await response.json()
-            console.log(resJson.results[1].book_author)    
+            console.log(resJson.results[1].book_author)
+            console.log(resJson.results[2].book_author)
+            console.log(resJson)
+               
         }
         ftechApi()
     })
     return(
         <>
             <h1>hello</h1>
+            <h2>edited</h2>
         </>
     )
 }
