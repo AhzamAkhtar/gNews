9a37a8949fb23d900cad0cbff0f347202a55bcca
Ahzam Akhtar <ahzam8186@gmail.com>
2021-11-19 16:06:55 +0530

    nyt

---
+++ src/Nyt.jsx
@@ -0,0 +1,18 @@
+import React,{useEffect} from "react";
+const Nyt=()=>{
+    useEffect(()=>{
+        const ftechApi=async()=>{
+            const url="https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=dlVR6N3WDcWAVYW05GQ5KmKlajpBJ0ck"
+            const response=await fetch(url)
+            const resJson=await response.json()
+            console.log(resJson)    
+        }
+        ftechApi()
+    })
+    return(
+        <>
+            <h1>hello</h1>
+        </>
+    )
+}
+export default Nyt
