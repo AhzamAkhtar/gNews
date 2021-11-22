739834ebaeceb8863f421e0628f6195052c81280
Ahzam Akhtar <ahzam8186@gmail.com>
2021-11-23 03:00:14 +0530

    gNewsApi

--- src/ImgSlider.jsx
+++ src/ImgSlider.jsx
@@ -12,21 +12,21 @@ const ImgSlider = () => {
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="https://picsum.photos/1700/403" class="d-block w-100" alt="..." />
+                        <img src="https://picsum.photos/1900/1003" class="d-block w-100" alt="..." />
                         <div class="carousel-caption d-none d-md-block">
                             <h5>First slide label</h5>
                             <p>Some representative placeholder content for the first slide.</p>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img src="https://picsum.photos/1700/402" class="d-block w-100" alt="..." />
+                        <img src="https://picsum.photos/1900/1002" class="d-block w-100" alt="..." />
                         <div class="carousel-caption d-none d-md-block">
                             <h5>Second slide label</h5>
                             <p>Some representative placeholder content for the second slide.</p>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img src="https://picsum.photos/1700/401" class="d-block w-100" alt="..." />
+                        <img src="https://picsum.photos/1900/1001" class="d-block w-100" alt="..." />
                         <div class="carousel-caption d-none d-md-block">
                             <h5>Third slide label</h5>
                             <p>Some representative placeholder content for the third slide.</p>
