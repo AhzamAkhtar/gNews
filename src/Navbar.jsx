ea713bb89ab40fce1ed70b7a05b68d1dd78a4340
Ahzam Akhtar <ahzam8186@gmail.com>
2021-11-23 02:42:27 +0530

    gNews

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
         <>
             <nav class="navbar navbar-dark bg-dark">
                 <div class="container-fluid">
-                    <span class="navbar-brand mb-0 h1">gNEWS.IO</span>
+                    <span class="navbar-brand mb-0 h1">gNEWS.io</span>
                 </div>
             </nav>
         </>
