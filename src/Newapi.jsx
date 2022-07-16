1f336b95af5e65dac06b6c56290d5a2fc77e100c
Ahzam Akhtar <ahzam8186@gmail.com>
2022-07-17 03:17:21 +0530

    done

--- src/Newapi.jsx
+++ src/Newapi.jsx
@@ -226,22 +226,19 @@ const Newapi = () => {
                     <div className="carousel-item active">
                         <img src="https://picsum.photos/1900/1003" className="d-block w-100" alt="..." />
                         <div className="carousel-caption d-none d-md-block">
-                            <h5>First slide label</h5>
-                            <p>Some representative placeholder content for the first slide.</p>
+                            
                         </div>
                     </div>
                     <div className="carousel-item">
                         <img src="https://picsum.photos/1900/1002" className="d-block w-100" alt="..." />
                         <div className="carousel-caption d-none d-md-block">
-                            <h5>Second slide label</h5>
-                            <p>Some representative placeholder content for the second slide.</p>
+                            
                         </div>
                     </div>
                     <div className="carousel-item">
                         <img src="https://picsum.photos/1900/1001" className="d-block w-100" alt="..." />
                         <div className="carousel-caption d-none d-md-block">
-                            <h5>Third slide label</h5>
-                            <p>Some representative placeholder content for the third slide.</p>
+                        
                         </div>
                     </div>
                 </div>
